eee76a6efb1822faa8b2b76575ea9a0aa8d394ae
K-S-A <atillas@ukr.net>
2016-03-07 20:32:12 +0200

    Add support for angular/devise csrf token update.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,24 @@ class ApplicationController < ActionController::Base
   def main
   end
 
+  respond_to :json
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  after_action :set_csrf_cookie_for_ng
+
+  protected
+
+  def verified_request?
+    super || valid_authenticity_token?(session, request.headers['X-XSRF-TOKEN'])
+  end
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :first_name << :last_name
+  end
+
+  def set_csrf_cookie_for_ng
+    cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
+  end
 end
