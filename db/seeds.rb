0bf92d3a743fb1b3dcabd417adf66e910fb94357
K-S-A <atillas@ukr.net>
2016-03-25 04:40:08 +0200

    Add migration for tasks; db seeds for basic projects/tasks.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,18 @@
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+user = User.create(email: 'atillas@ukr.net', password: '321321321', first_name: 'John', last_name: 'Snow')
+
+user.projects.create([{title: 'For Home'}, {title: 'Complete the test task for Ruby Garage'}])
+
+user.projects.first.tasks.create([
+  {name: 'Open this mock-up in Adobe Fireworks'},
+  {name: 'Attentively check the file'},
+  {name: 'Write HTML & CSS'},
+  {name: 'Add Javascript to implement adding / editing / removing for todo items and lists taking into account as more use cases as possible'}
+])
+
+# # TODO - uncomment
+# user.projects.first.tasks.create([
+#   {name: 'Buy a milk'},
+#   {name: 'Call Mam'},
+#   {name: 'Clean the room'},
+#   {name: 'Repair the DVD Player'}
+# ])
