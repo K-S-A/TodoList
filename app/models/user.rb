e2da8ba3897d1fefb6ff22d41fbc50fd5dc7a269
K-S-A <atillas@ukr.net>
2016-03-28 02:18:10 +0300

    Add has_many/through relation User => Comments; specs.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   has_many :projects
   has_many :tasks, through: :projects
+  has_many :comments, through: :tasks
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,4 +3,5 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   it { expect(subject).to have_many(:projects) }
   it { expect(subject).to have_many(:tasks).through(:projects) }
+  it { expect(subject).to have_many(:comments).through(:tasks) }
 end
