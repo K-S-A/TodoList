896e93a6471df83f45997a09621c1674bee9fdf8
K-S-A <atillas@ukr.net>
2016-03-27 04:03:10 +0300

    Add ranked model to Task; update task_params in controller.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -26,6 +26,6 @@ class TasksController < ApplicationController
   end
 
   def task_params
-    params.require(:task).permit(:name)
+    params.require(:task).permit(:name, :project_id, :priority_position)
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,12 @@
 class Task < ActiveRecord::Base
+  include RankedModel
+
   belongs_to :project
+  ranks :priority, with_same: :project_id
 
   validates :name, presence: true,
                    uniqueness: { scope: :project_id,
                                  case_sensitive: false }
+
+  default_scope { rank(:priority) }
 end
